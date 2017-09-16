afb0fa8e12f0b3377c0ffa1f05aada6189e63f9c
BeerBC16 <beerbc16@gmail.com>
2017-09-16 16:03:48 +0700

    Finish of first day

--- app/src/main/java/app/security/egat/myservice/fragment/SecondFragment.java
+++ app/src/main/java/app/security/egat/myservice/fragment/SecondFragment.java
@@ -14,35 +14,29 @@ import app.security.egat.myservice.R;
  * Created by Phakpoom.I on 16/9/2560.
  */
 
-public class MainFragment extends Fragment {
+public class SecondFragment extends Fragment{
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_main, container, false); //Create a new view from xml
+        View view = inflater.inflate(R.layout.fragment_second, container, false);
         return view;
-    }//Create Fragment View
+    }
 
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
-
-        //Button controller
         buttonController();
-
     }
 
     private void buttonController() {
-        Button button = (Button)getView().findViewById(R.id.btnGoToSecond);
-        button.setOnClickListener(new View.OnClickListener() {
-            //เมื่อคลิกจะสร้าง obj onclickListener และ Override Event Click
+        Button button = (Button)getView().findViewById(R.id.btnGoToMain);
+        button.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v) {
                 getActivity().getSupportFragmentManager()
-                        .beginTransaction()
-                        .replace(R.id.contentFragmentMain,new SecondFragment())
-                        .addToBackStack(null)
-                        .commit();
+                        .popBackStack();
             }
         });
     }
-}
+}   //Main class
